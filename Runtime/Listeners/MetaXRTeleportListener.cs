bf2f8546a938166124222011b24fb6592a832a8c
Marc Troll <marc.troll@rwth-aachen.de>
2025-06-10 15:37:37 +0200

    add listener and tracking behaviour for MetaXR Teleportation

---
+++ Runtime/Listeners/MetaXRTeleportListener.cs
@@ -0,0 +1,16 @@
+﻿using Oculus.Interaction.Locomotion;
+using OmiLAXR.Listeners;
+
+namespace OmiLAXR.MetaXR.Listeners
+{
+    public class MetaXRTeleportListener : Listener
+    {
+        public override void StartListening()
+        {
+            print("StartListening MetaXR Teleport");
+            var gos = FindObjects<TeleportInteractor>();
+            print("FOUND GOs: " + gos);
+            Found(gos);
+        }
+    }
+}
