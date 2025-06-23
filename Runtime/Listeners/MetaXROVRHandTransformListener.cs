b70cafad5e48c3f31a3fa6c8b4d31a607bf10e18
Marc Troll <marc.troll@rwth-aachen.de>
2025-06-23 09:31:25 +0200

    removed unused Tracking Behaviours, Components, and Composers

---
+++ Runtime/Listeners/MetaXROVRHandTransformListener.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using OmiLAXR.Listeners;
+using UnityEngine;
+
+namespace OmiLAXR.MetaXR.Listeners
+{
+    [AddComponentMenu("OmiLAXR / 1) Listeners / MetaXR <OVRHand> Objects Listener"),
+     Description("Provides all <OVRHand> components (left or right hand) to pipeline.")]
+    public class MetaXROVRHandTransformListener : Listener
+    {
+        public override void StartListening()
+        {
+            var hands = FindObjects<OVRHand>();
+            foreach (var hand in hands)
+            {
+                if (hand.GetHand() == OVRPlugin.Hand.HandLeft || hand.GetHand() == OVRPlugin.Hand.HandRight)
+                {
+                    // TODO: Check for name "LeftHandAnchor" / "RightHandAnchor"?
+                    var tw = hand.gameObject.transform.parent.gameObject.GetComponent<TransformWatcher>() ?? hand.transform.parent.gameObject.AddComponent<TransformWatcher>();
+                    Found(tw);
+                }
+            }
+        }
+    }
+}
