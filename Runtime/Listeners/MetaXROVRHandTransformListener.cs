5a531b32c41d51eab56117b8665281e60d76753b
Marc Troll <marc.troll@rwth-aachen.de>
2025-06-23 10:25:08 +0200

    add possibility to ignore single hands for hand tracking

--- Runtime/Listeners/MetaXROVRHandTransformListener.cs
+++ Runtime/Listeners/MetaXROVRHandTransformListener.cs
@@ -1,13 +1,25 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
+using Codice.CM.Common;
 using OmiLAXR.Listeners;
 using UnityEngine;
 
 namespace OmiLAXR.MetaXR.Listeners
 {
+    
+    [Serializable]
+    public struct HandIgnore
+    {
+        public bool HandLeft;
+        public bool HandRight;
+    }
+    
     [AddComponentMenu("OmiLAXR / 1) Listeners / MetaXR <OVRHand> Objects Listener"),
      Description("Provides all <OVRHand> components (left or right hand) to pipeline.")]
     public class MetaXROVRHandTransformListener : Listener
     {
+        public HandIgnore ignore;
+        
         public override void StartListening()
         {
             var hands = FindObjects<OVRHand>();
@@ -16,8 +28,17 @@ namespace OmiLAXR.MetaXR.Listeners
                 if (hand.GetHand() == OVRPlugin.Hand.HandLeft || hand.GetHand() == OVRPlugin.Hand.HandRight)
                 {
                     // TODO: Check for name "LeftHandAnchor" / "RightHandAnchor"?
-                    var tw = hand.gameObject.transform.parent.gameObject.GetComponent<TransformWatcher>() ?? hand.transform.parent.gameObject.AddComponent<TransformWatcher>();
-                    Found(tw);
+                    if (hand.GetHand() == OVRPlugin.Hand.HandLeft && !ignore.HandLeft)
+                    {
+                        var tw = hand.gameObject.transform.parent.gameObject.GetComponent<TransformWatcher>() ?? hand.transform.parent.gameObject.AddComponent<TransformWatcher>();
+                        Found(tw);
+                    }
+                    
+                    if (hand.GetHand() == OVRPlugin.Hand.HandRight && !ignore.HandRight)
+                    {
+                        var tw = hand.gameObject.transform.parent.gameObject.GetComponent<TransformWatcher>() ?? hand.transform.parent.gameObject.AddComponent<TransformWatcher>();
+                        Found(tw);
+                    }
                 }
             }
         }
