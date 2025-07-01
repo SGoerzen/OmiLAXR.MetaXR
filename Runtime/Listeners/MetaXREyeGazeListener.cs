fc5a3b0fbd0ab4fbf36c3d2bb91184a16268a305
Marc Troll <marc.troll@rwth-aachen.de>
2025-07-01 14:57:57 +0200

    add eye gaze listener

---
+++ Runtime/Listeners/MetaXREyeGazeListener.cs
@@ -0,0 +1,20 @@
+﻿using OmiLAXR.Listeners;
+
+namespace OmiLAXR.MetaXR.Listeners
+{
+    public class MetaXREyeGazeListener : Listener
+    {
+        public override void StartListening()
+        {
+            var eyeGazes = FindObjects<OVREyeGaze>();
+            foreach (var eyeGaze in eyeGazes)
+            {
+                if (eyeGaze.EyeTrackingEnabled)
+                {
+                    var tw = eyeGaze.gameObject.transform.gameObject.GetComponent<TransformWatcher>() ?? eyeGaze.transform.gameObject.AddComponent<TransformWatcher>();
+                    Found(tw);
+                }
+            }
+        }
+    }
+}
