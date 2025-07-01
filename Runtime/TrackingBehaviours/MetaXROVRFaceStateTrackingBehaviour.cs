9851fa5fc5a04ca4da82510566dc50662b0c5115
Marc Troll <marc.troll@rwth-aachen.de>
2025-07-01 15:14:14 +0200

    add basics for OVR Face State Tracking

---
+++ Runtime/TrackingBehaviours/MetaXROVRFaceStateTrackingBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using OmiLAXR.Schedules;
+using OmiLAXR.TrackingBehaviours;
+
+namespace OmiLAXR.MetaXR.TrackingBehaviours
+{
+    public class MetaXROVRFaceStateTrackingBehaviour : TrackingBehaviour
+    {
+        public IntervalTicker.Settings intervalSettings;
+        
+        private OVRPlugin.FaceState _currentFaceState;
+        
+        public readonly TrackingBehaviourEvent<bool> OnFaceStateTracking =
+            new TrackingBehaviourEvent<bool>();
+        
+        protected override void OnStartedPipeline(Pipeline pipeline)
+        {
+            SetInterval(() =>
+            {
+                OnFaceStateTracking.Invoke(this, OVRPlugin.GetFaceState(OVRPlugin.Step.Render, -1, ref _currentFaceState));
+            }, intervalSettings);
+        }
+    }
+}
