97b660ccb75d4f86a61fb85bf155078c8120ffba
Sergej Görzen <goerzen@cs.rwth-aachen.de>
2025-08-01 16:05:03 +0200

    finish eye hover via raycast

--- Runtime/Listeners/MetaXROVREyeGazeListener.cs
+++ Runtime/Listeners/MetaXROVREyeGazeListener.cs
@@ -1,25 +1,35 @@
 ﻿using System.ComponentModel;
 using OmiLAXR.Listeners;
-using OmiLAXR.TrackingBehaviours.Learner.EyeTracking;
+using OmiLAXR.Types;
 using UnityEngine;
 
 namespace OmiLAXR.MetaXR.Listeners
 {   
     [AddComponentMenu("OmiLAXR / 1) Listeners / MetaXR <OVREyeGaze> Objects Listener"),
       Description("Provides all <OVREyeGaze> components to pipeline.")]
-    public class MetaXROVREyeGazeListener : Listener
+    public class MetaXROVREyeGazeListener : EyeInteractorListener<OVREyeGaze>
     {
-        public override void StartListening()
+        // public override bool IsEyeTrackingEnabled(OVREyeGaze t)
+        // {
+        //     var eyeState = new OVRPlugin.EyeGazesState();
+        //     
+        //     if (OVRPlugin.GetEyeGazesState(OVRPlugin.Step.Render, -1, ref eyeState))
+        //     {
+        //         return eyeState.EyeGazes[(int)t.Eye].IsValid;
+        //     }
+        //
+        //     return false;
+        // }
+        public override Eye GetEyeSide(OVREyeGaze t)
         {
-            var eyeGazes = FindObjects<OVREyeGaze>();
-            foreach (var eyeGaze in eyeGazes)
+            switch (t.Eye)
             {
-                if (!eyeGaze.EyeTrackingEnabled) 
-                    continue;
-                var tw = eyeGaze.gameObject.transform.gameObject.GetComponent<TransformWatcher>() ?? eyeGaze.transform.gameObject.AddComponent<TransformWatcher>();
-                Found(tw);
-                var ei = eyeGaze.gameObject.GetComponent<EyeInteractor>() ?? eyeGaze.gameObject.AddComponent<EyeInteractor>();
-                Found(ei);
+                case OVREyeGaze.EyeId.Left:
+                    return Eye.Left;
+                case OVREyeGaze.EyeId.Right:
+                    return Eye.Right;
+                default:
+                    return Eye.Unknown;
             }
         }
     }
