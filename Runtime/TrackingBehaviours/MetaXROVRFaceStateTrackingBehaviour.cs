0142350c5384d422b406278eeb3396c5fdffd245
Sergej Görzen <goerzen@cs.rwth-aachen.de>
2025-09-06 15:37:33 +0200

    added face feature

--- Runtime/TrackingBehaviours/MetaXROVRFaceStateTrackingBehaviour.cs
+++ Runtime/TrackingBehaviours/MetaXROVRFaceStateTrackingBehaviour.cs
@@ -1,28 +1,32 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using OmiLAXR.Components;
+using OmiLAXR.MetaXR.Components;
 using OmiLAXR.TrackingBehaviours;
 using UnityEngine;
 
 namespace OmiLAXR.MetaXR.TrackingBehaviours
 {
-    
-    [AddComponentMenu("OmiLAXR / 3) Tracking Behaviours / [MetaXR] <FaceState> Tracking Behaviour"), 
+    [AddComponentMenu("OmiLAXR / 3) Tracking Behaviours / [MetaXR] <FaceState> Tracking Behaviour"),
      Description("Tracks Face State (untested, in development).")]
-    public sealed class MetaXROVRFaceStateTrackingBehaviour : ScheduledTrackingBehaviour
+    public sealed class MetaXROVRFaceStateTrackingBehaviour : FacialTrackingBehaviour
     {
         private OVRPlugin.FaceState _currentFaceState;
-        
-        protected override void Run()
+
+        public override bool IsAvailable => OVRPlugin.faceTrackingSupported || OVRPlugin.faceTracking2Supported;
+        public override bool IsEnabled => OVRPlugin.faceTrackingEnabled || OVRPlugin.faceTracking2Enabled;
+
+        protected override FaceData FetchFaceData()
         {
-            var isEnabled = OVRPlugin.faceTrackingEnabled && OVRPlugin.faceTrackingSupported ||
-                             OVRPlugin.faceTracking2Enabled && OVRPlugin.faceTracking2Supported;
-            if (!isEnabled)
-            {
-                StopSchedules();
-                return;
-            }
-            
+            // Query latest face state from OVR
             OVRPlugin.GetFaceState(OVRPlugin.Step.Render, -1, ref _currentFaceState);
-            Debug.Log(_currentFaceState);
+
+            // Wrap as FaceData for your existing pipeline
+            return new MetaXROVRFaceData(
+                _currentFaceState.ExpressionWeights,
+                _currentFaceState.ExpressionWeightConfidences
+            );
         }
     }
 }
